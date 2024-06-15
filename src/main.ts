2011613dc16c04e37536fb38362a0e4746fa2072
andreycaetano <andreycaetano2102@gmail.com>
2024-06-15 20:52:34 -0300

    feat(errorFilter): create and apply global filter exception

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './errors/allExceptionsFielter.error';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({
